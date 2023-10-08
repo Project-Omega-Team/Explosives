9ded49bae8ed47a2b489081d38d4c0ecb3509639
Leinier Perez <P_leinier@yahoo.com>
2023-10-08 06:09:18 -0400

    set up project

---
+++ src/main/java/io/github/projectomega/explosives/Explosives.java
@@ -0,0 +1,8 @@
+package io.github.projectomega.explosives;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Explosives.MOD_ID)
+public class Explosives {
+    public static final String MOD_ID = "projectomega_explosives";
+}
